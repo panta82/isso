d8bded09613152b31e9f35ac43641e4762be206e
Ivan Pantic <ivanpantic82@gmail.com>
2016-12-17 22:55:05 +0100

    More tweaking

--- misc/free-bsd-isso.sh
+++ misc/free-bsd-isso.sh
@@ -23,20 +23,9 @@ load_rc_config $name
 pidfile=${isso_pidfile}
 required_files="${isso_config}"
 command=/usr/sbin/daemon
-command_args="-f -r -P ${isso_pidfile} -u pantas /home/pantas/isso/misc/logwrapper.sh '${isso_logout}'"\
- "'${isso_logerr}' /home/pantas/.pyvenv/isso/bin/isso -c '${isso_config}'"
+command_args="-f -r -P ${isso_pidfile} -u pantas /home/pantas/isso/misc/logwrapper.sh '${isso_logout}' "\
+"'${isso_logerr}' /home/pantas/.pyvenv/isso/bin/isso -c '${isso_config}'"
 
 export PATH="/home/pantas/.pyvenv/isso/bin:/home/pantas/local/bin:/home/pantas/.pyenv/bin:${PATH}"
-
-start_precmd="isso_start_precmd"
-
-isso_start_precmd()
-{
-    cd /home/pantas/isso
-
-    echo "------------------------------------------------------------------------------------------------------" >> "$isso_logout"
-    echo "service start ${name} at $(date)" >> "$isso_logout"
-    echo "${command} ${command_args}" >> "$isso_logout"
-}
 
 run_rc_command "$@"
