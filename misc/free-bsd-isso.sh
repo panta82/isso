e8547099c8eb4df7132e67e0b65dd0b516abe6d6
Ivan Pantic <ivanpantic82@gmail.com>
2016-12-18 00:33:06 +0100

    Got rid of log wrapper for now

--- misc/free-bsd-isso.sh
+++ misc/free-bsd-isso.sh
@@ -17,14 +17,11 @@ load_rc_config $name
 : ${isso_enable="NO"}
 : ${isso_config="/home/pantas/local/etc/isso.ini"}
 : ${isso_pidfile="/usr/local/var/run/isso.pid"}
-: ${isso_logout="/home/pantas/local/var/log/isso_out.log"}
-: ${isso_logerr="/home/pantas/local/var/log/isso_err.log"}
 
 pidfile=${isso_pidfile}
 required_files="${isso_config}"
 command=/usr/sbin/daemon
-command_args="-f -r -P ${isso_pidfile} -u pantas /home/pantas/isso/misc/logwrapper.sh '${isso_logout}' "\
-"'${isso_logerr}' /home/pantas/.pyvenv/isso/bin/isso -c '${isso_config}'"
+command_args="-f -r -P ${isso_pidfile} -u pantas /home/pantas/.pyvenv/isso/bin/isso -c '${isso_config}'"
 
 export PATH="/home/pantas/.pyvenv/isso/bin:/home/pantas/local/bin:/home/pantas/.pyenv/bin:${PATH}"
 
